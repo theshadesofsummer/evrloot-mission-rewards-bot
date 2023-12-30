96f7477bb6b9cd1171f425fb52ef800a978e0670
theshadesofsummer <louisschulze03@gmail.com>
2023-12-30 19:35:33 +0100

    summary handles rewards without rarity

--- src/summary/generate-summary.js
+++ src/summary/generate-summary.js
@@ -6,16 +6,26 @@ module.exports = function generateSummary() {
     if (stats.size <= 0) {
         return '`No items gathered in the last 24 hours`'
     }
-
-    const sortedStats = Array.from(stats).sort(raritySorter)
+    const allStats = Array.from(stats)
+    const sortedStatsWithRarity =
+      allStats.filter(hasSelectedRarity)
+      .sort(raritySorter)
 
     let summary = '```ansi\n' +
         `Missions claimed: \u001b[4;36m${missionCounter}\u001b[0m\n\n` +
         'Gathered in the last 24 hours:\n';
 
-    for (const [itemName, value] of sortedStats) {
+    for (const [itemName, value] of sortedStatsWithRarity) {
         const rarityColor = getColorRarity(value.rarity)
         summary += `${rarityColor}${itemName}\u001b[0m: ${value.amount}\n`
+    }
+
+    const sortedStatsWithoutRarity =
+      allStats.filter(item => !hasSelectedRarity(item))
+    summary += `\nRewards with unrevealed Rarity:\n`
+
+    for (const [itemName, value] of sortedStatsWithoutRarity) {
+        summary += `\u001b[1;36m${itemName}\u001b[0m: ${value.amount}\n`
     }
     summary += '```'
     
@@ -48,4 +58,9 @@ function raritySorter(entryA, entryB) {
     const rarityB = entryB[1].rarity;
 
     return raritySortValue.get(rarityA) - raritySortValue.get(rarityB)
+}
+
+function hasSelectedRarity(entry) {
+    const entryRarity = entry.rarity
+    return ['Common', 'Rare', 'Epic', 'Legendary'].includes(entryRarity)
 }
