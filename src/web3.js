b829f19533016993c68904bed434fcbb053fb027
theshadesofsummer <louisschulze03@gmail.com>
2024-04-21 19:48:04 +0200

    switch to moonbeam rpc

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
 
 module.exports = new Web3(
-  new Web3.providers.WebsocketProvider('wss://moonbase-alpha.blastapi.io/3f7856cf-73cf-489e-9973-0daafbd333a6', {
+  new Web3.providers.WebsocketProvider('wss://moonbeam.blastapi.io/3f7856cf-73cf-489e-9973-0daafbd333a6', {
     clientConfig: {
       keepalive: true,
       keepaliveInterval: 60000,
