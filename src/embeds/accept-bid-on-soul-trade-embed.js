c5c600daee8803955164f609d91acdc6e72a2e29
theshadesofsummer <louisschulze03@gmail.com>
2024-12-17 19:25:52 +0100

    added soul trade success

---
+++ src/embeds/accept-bid-on-soul-trade-embed.js
@@ -0,0 +1,95 @@
+const {getAuthor, getFields} = require("../trades/embed-helpers");
+const {findClassEmote} = require("../helpers/emotes");
+
+module.exports = function createSoulNewTradeEmbed(tradeInfo, textInfo, tradeCreator, soul, imageName, bidInfo, bidCreator, bidNfts, bidResources) {
+  const author = getAuthor(tradeCreator)
+  const fields = getFields(tradeInfo, [], [], true)
+
+  const bidFields = getFields(bidInfo, bidNfts, bidResources, false)
+
+  const titlePrefix = `${bidCreator ? `${bidCreator.globalName}'s ` : ''}`
+  const title = `${titlePrefix}Bid Accepted: ${findClassEmote(soul.retrievedMetadata.properties['Soul Class'].value)}${textInfo.title}`
+
+  return {
+    color: 0x2f9f00,
+    title: title,
+    url: `https://game.evrloot.com/marketplace?mId=${tradeInfo.id}`,
+    author,
+    image: {
+     url: `attachment://${imageName}`
+    },
+    description: textInfo.message,
+    fields: [
+      ...fields,
+      {
+        name: 'Experience',
+        value: soulExperienceFormatter(soul.experience.activities),
+        inline: true
+      },
+      {
+        name: 'Equipment',
+        value: soulChildsFormatter(soul.children),
+        inline: true
+      },
+      {
+        name: 'Properties',
+        value: soulStatsFormatter(soul.retrievedMetadata.properties),
+        inline: false
+      },
+      ...bidFields
+    ],
+    timestamp: new Date().toISOString(),
+  };
+}
+
+function soulStatsFormatter(properties) {
+  let returnString = '';
+
+  Object.entries(properties).forEach(([name, property]) => {
+    returnString += `*${name}*: ${property.value}\n`;
+  })
+
+  return returnString;
+}
+
+const shownExperiences = [1, 2, 3, 4, 6, 7, 9]
+
+function soulExperienceFormatter(experiences) {
+  const expStrings = experiences
+    .filter(exp => shownExperiences.includes(exp.activityId))
+    .map(getExpString)
+
+  let returnString = '';
+
+  expStrings.forEach(expString => returnString += expString)
+
+  return returnString;
+}
+
+function getExpString(exp) {
+  return `*${exp.activityName}*: ${exp.experience}\n`
+}
+
+function soulChildsFormatter(children) {
+  let returnString = '';
+  children
+    .sort(raritySorter)
+    .forEach(child => returnString += `[${child.retrievedMetadata.properties["Rarity"].value}] *${child.retrievedMetadata.name}*\n`)
+
+  return returnString;
+}
+
+const raritySortValue = new Map([
+  ['Common', 0],
+  ['Rare', 1],
+  ['Epic', 2],
+  ['Legendary', 3],
+])
+
+function raritySorter(entryA, entryB) {
+  const rarityA = entryA.retrievedMetadata.properties["Rarity"].value;
+  const rarityB = entryB.retrievedMetadata.properties["Rarity"].value;
+
+  return raritySortValue.get(rarityA) - raritySortValue.get(rarityB)
+}
+
--- src/trades/handle-bid-accepted.js
+++ src/trades/handle-bid-accepted.js
@@ -2,9 +2,9 @@ const {
   fetchTradeByIdFromSquid,
   getFromIpfs,
   fetchNftMetadataByIdAndCollection,
-  fetchBidByIdFromSquid
+  fetchBidByIdFromSquid, getCitizenImage, getSoulImage
 } = require("../evrloot-api");
-const {postNewTrade, getUserByClientId, logMessageOrError} = require("../discord-client");
+const {postNewTrade, getUserByClientId, logMessageOrError, postNewTradeWithImage} = require("../discord-client");
 const createBidAcceptedEmbed = require('../embeds/bid-accepted-embed')
 const resourceRewards = require("../mappings/resource-types");
 const {getTradeMessages, getAccountByWallet} = require("../evrloot-db");
@@ -12,8 +12,11 @@ const {
   getTradeNfts,
   getDiscordUserForWallet,
   getNftWithMetadata,
-  getResourcesWithMetadata
+  getResourcesWithMetadata, getSoulMetadataForTrade
 } = require("./trade-helpers");
+const fs = require("fs");
+const {AttachmentBuilder} = require("discord.js");
+const createBidAcceptedSoulTradeEmbed = require("../embeds/accept-bid-on-soul-trade-embed");
 
 module.exports = {
   handleBidAccepted
@@ -24,8 +27,8 @@ async function handleBidAccepted(tradeId) {
   try {
     await new Promise(resolve => setTimeout(resolve, 10000));
     const tradeInfo = await fetchTradeByIdFromSquid(tradeId)
-    console.log('no trade info found')
     if (!tradeInfo) {
+      console.log('no trade info found')
       return;
     }
 
@@ -35,24 +38,82 @@ async function handleBidAccepted(tradeId) {
     const tradeCreator = await getDiscordUserForWallet(tradeInfo.ownerAddress)
     console.log('1')
 
-    const tradeNfts = await getNftWithMetadata(tradeInfo.erc721s, tradeInfo.unclaimedNfts)
-    const tradeResources = await getResourcesWithMetadata(tradeInfo.erc1155s, tradeInfo.unclaimedResources)
-    console.log('2')
+    if (isSoulTrade(tradeInfo)) {
+      console.log('is soul trade true for bid win')
+      const soul = await getSoulMetadataForTrade(tradeInfo);
 
-    const bidId = tradeInfo.acceptedBid.id
-    const bidInfo = await fetchBidByIdFromSquid(bidId)
-    console.log('3')
-    const bidCreator = await getDiscordUserForWallet(bidInfo.ownerAddress)
+      let soulImageBuffer;
+      let imageFileName;
+      if (soul.id.startsWith("CITIZENS")) {
+        soulImageBuffer = await getCitizenImage(soul.tokenId);
+        imageFileName = `citizen_${soul.tokenId}.png`;
+      } else {
+        soulImageBuffer = await getSoulImage(soul.tokenId)
+        imageFileName = `soul_${soul.tokenId}.png`;
+      }
 
-    const bidNfts = await getNftWithMetadata(bidInfo.erc721s, bidInfo.unclaimedNfts)
-    const bidResources = await getResourcesWithMetadata(bidInfo.erc1155s, bidInfo.unclaimedResources)
+      let path = 'static/'+imageFileName;
+      fs.createWriteStream(path).write(soulImageBuffer);
+      fs.createWriteStream(path).close();
 
-    const bidAcceptedEmbed = createBidAcceptedEmbed(tradeInfo, textInfo, tradeCreator, tradeNfts, tradeResources, bidInfo, bidCreator, bidNfts, bidResources)
+      const attachments = new AttachmentBuilder()
+        .setFile(path)
+        .setName(imageFileName)
 
-    await postNewTrade(bidAcceptedEmbed)
+      const bidId = tradeInfo.acceptedBid.id
+      const bidInfo = await fetchBidByIdFromSquid(bidId)
+      const bidCreator = await getDiscordUserForWallet(bidInfo.ownerAddress)
+
+      const bidNfts = await getNftWithMetadata(bidInfo.erc721s, bidInfo.unclaimedNfts)
+      const bidResources = await getResourcesWithMetadata(bidInfo.erc1155s, bidInfo.unclaimedResources)
+
+      const newTradeEmbed = createBidAcceptedSoulTradeEmbed(tradeInfo, textInfo, tradeCreator, soul, imageFileName, bidInfo, bidCreator, bidNfts, bidResources)
+      await postNewTradeWithImage(newTradeEmbed, attachments)
+
+      fs.unlink(path, (err) => {
+        if (err) {
+          console.error('Error removing file:', err);
+          return;
+        }
+        console.log('File removed successfully:', path);
+      });
+    } else {
+      const tradeNfts = await getNftWithMetadata(tradeInfo.erc721s, tradeInfo.unclaimedNfts)
+      const tradeResources = await getResourcesWithMetadata(tradeInfo.erc1155s, tradeInfo.unclaimedResources)
+      console.log('2')
+
+      const bidId = tradeInfo.acceptedBid.id
+      const bidInfo = await fetchBidByIdFromSquid(bidId)
+      console.log('3')
+      const bidCreator = await getDiscordUserForWallet(bidInfo.ownerAddress)
+
+      const bidNfts = await getNftWithMetadata(bidInfo.erc721s, bidInfo.unclaimedNfts)
+      const bidResources = await getResourcesWithMetadata(bidInfo.erc1155s, bidInfo.unclaimedResources)
+
+      const bidAcceptedEmbed = createBidAcceptedEmbed(tradeInfo, textInfo, tradeCreator, tradeNfts, tradeResources, bidInfo, bidCreator, bidNfts, bidResources)
+
+      await postNewTrade(bidAcceptedEmbed)
+
+    }
+
+
   } catch (e) {
     await logMessageOrError('could not send trade accepted event')
     console.error(e)
   }
 
+}
+
+const estraTokenNftAddress = '0xba6bd2aace40c9a14c4123717119a80e9fe6738a' // identify if soul is staked => fetch via estra token
+const soulNftAddress = '0x9d1454e198f4b601bfc0069003045b0cbc0e6749'
+
+function isSoulTrade(tradeInfo) {
+  console.log('is soul trade on bid win')
+  if (tradeInfo.erc1155s.length > 0 || tradeInfo.unclaimedResources.length > 0) return false
+  console.log('not 1', tradeInfo.erc721s.length, tradeInfo.unclaimedNfts.length)
+  if (tradeInfo.erc721s.length + tradeInfo.unclaimedNfts.length !== 1) return false
+  console.log('not 2')
+  if (tradeInfo.erc721s[0]) return tradeInfo.erc721s[0].contractAddress.toLowerCase() === estraTokenNftAddress
+  console.log('not 3')
+  return tradeInfo.unclaimedNfts[0].contractAddress.toLowerCase() === soulNftAddress
 }
