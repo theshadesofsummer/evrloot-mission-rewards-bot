2f9fbb45dea6ef4218c0de1d59583381df32799f
Brandon Macer <bmacer@cisco.com>
2025-12-28 14:37:34 -0500

    update public websocket rpc

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3');
+// Moonbeam official public WebSocket RPC: https://docs.moonbeam.network/builders/get-started/endpoints/
 module.exports = new Web3(
-  new Web3.providers.WebsocketProvider('wss://moonbeam.blastapi.io/3f7856cf-73cf-489e-9973-0daafbd333a6', {
+  new Web3.providers.WebsocketProvider('wss://wss.api.moonbeam.network', {
     clientConfig: {
       keepalive: true,
       keepaliveInterval: 60000,
